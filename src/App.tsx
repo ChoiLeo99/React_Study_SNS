8633ef122bf326d745b6304559668f4ba8b6b03d
ChoiLeo99 <choileo0910@gmail.com>
2025-12-02 23:28:01 +0900

    프로젝트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <div className="text-xs text-red-500">text-xs</div>
+      <div className="text-sm text-[rgb(100,200,300)]">text-sm</div>
+      <div className="text-lg font-black">text-lg</div>
+      <div className="text-[13px] font-extrabold">text-13px</div>
+    </div>
+  );
 }
 
 export default App;
